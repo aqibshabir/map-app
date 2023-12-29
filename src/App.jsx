bffed2a894cb1e432f01a0cfca6dfe41366f5d2a
Aqib Shabir <aqib@Aqibs-MacBook-Air.local>
2023-12-29 17:35:55 +0000

    integrated mapbox

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,16 @@ function App() {
       <ReactMapGL
         mapboxAccessToken={key}
         initialViewState={{
-          longitude: -1.2052,
-          latitude: 52.633331,
-          zoom: 4.5,
+          longitude: -2.5209,
+          latitude: 53.945,
+          zoom: 5,
         }}
         style={{
           position: "absolute",
           width: "100vw",
           height: "100vh",
         }}
-        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapStyle="mapbox://styles/mapbox/light-v10"
       />
     </>
   );
