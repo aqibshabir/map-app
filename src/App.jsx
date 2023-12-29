931bc7be7fe563e7346752585a2854c63915de9b
Aqib Shabir <aqib@Aqibs-MacBook-Air.local>
2023-12-29 16:42:30 +0000

    integrated mapbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import './App.css'
+import ReactMapGL from "react-map-gl";
+import "./App.css";
 
 function App() {
-
+  const key = import.meta.env.VITE_MAPBOX;
 
   return (
     <>
-<h1>Map app</h1>
+      <ReactMapGL
+        mapboxAccessToken={key}
+        initialViewState={{
+          longitude: -1.2052,
+          latitude: 52.633331,
+          zoom: 4.5,
+        }}
+        style={{
+          position: "absolute",
+          width: "100vw",
+          height: "100vh",
+        }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
